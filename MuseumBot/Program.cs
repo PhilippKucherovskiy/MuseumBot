11fef59dfb42237af8dd6ad345add58beae9d1d4
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-01-22 20:23:29 +0300

    Просто экскурсия

--- MuseumBot/Program.cs
+++ MuseumBot/Program.cs
@@ -1,35 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Telegram.Bot;
-using Telegram.Bot.Args;
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
-using System.Xml.Serialization;
-using System.IO;
+using System.Threading.Tasks;
 
 namespace MuseumBot
 {
     class Program
     {
         private static TelegramBotClient _bot;
-        private static List<User> _users;
-        private static XmlSerializer _serializer;
-        private static string _fileName = "users.xml";
+        private static int _currentPage = 0;
+        private static string[] _pages = { "page1.jpg", "page2.jpg", "page3.jpg" };
+        private static string[] _captions = { "Caption 1", "Caption 2", "Caption 3" };
+        private static string[] _texts = { "Text 1", "Text 2", "Text 3" };
 
         static void Main(string[] args)
         {
             _bot = new TelegramBotClient("5981544709:AAGjFu0jUn0jrYWHeR3wq4BDCmLOQus1VAo");
             _bot.OnCallbackQuery += BotOnCallbackQueryReceived;
-
-            _serializer = new XmlSerializer(typeof(List<User>));
-
-            // Deserialize from XML file
-            using (FileStream fs = new FileStream(_fileName, FileMode.OpenOrCreate))
-            {
-                _users = (List<User>)_serializer.Deserialize(fs);
-            }
-
             _bot.StartReceiving();
             Console.ReadLine();
             _bot.StopReceiving();
@@ -37,74 +24,54 @@ namespace MuseumBot
 
         private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs e)
         {
-            string buttonText = e.CallbackQuery.Data;
-            long chatId = e.CallbackQuery.Message.Chat.Id;
+            var buttonText = e.CallbackQuery.Data;
+            var chatId = e.CallbackQuery.Message.Chat.Id;
 
-            switch (buttonText)
+            if (buttonText == "Start Tour")
             {
-                case "Tour":
-                    await SendTour(chatId);
-                    break;
-                case "Sign up":
-                    await SignUp(chatId);
-                    break;
-                case "Feedback":
-                    await Feedback(chatId);
-                    break;
+                await StartTour(chatId);
+            }
+            else if (buttonText == "Next")
+            {
+                await SendNextPage(chatId);
             }
         }
-
-        private static async Task SendTour(long chatId)
+        private static async Task StartTour(long chatId)
         {
-            // Send images and text for the tour
-            await _bot.SendPhotoAsync(chatId, "image1.jpg", "Caption 1");
-            await _bot.SendMessageAsync(chatId, "Text 1");
-            await _bot.SendPhotoAsync(chatId, "image2.jpg", "Caption 2");
-            await _bot.SendMessageAsync(chatId, "Text 2");
-            // Repeat as necessary
+            // Send initial message with a button to start the tour
+            var keyboard = new InlineKeyboardMarkup(new[]
+            {
+            new [] // first row
+            {
+                InlineKeyboardButton.WithCallbackData("Start Tour")
+            },
+        });
+            await _bot.SendMessageAsync(chatId, "Добро пожаловать на обзорную экскурсию по музею!", replyMarkup: keyboard);
         }
 
-        private static async Task SignUp(long chatId)
+        private static async Task SendNextPage(long chatId)
         {
-            // Ask for user details
-            await _bot.SendMessageAsync(chatId, "Сколько человек собирается посетить музей?");
-            var peopleCount = int.Parse(await _bot.GetCallbackQueryAsync(chatId));
-            await _bot.SendMessageAsync(chatId, "Какая дата Вашего визита в наш музей");
-            var date = DateTime.Parse(await _bot.GetCallbackQueryAsync(chatId));
-            await _bot.SendMessageAsync(chatId, "Какое время Вашего посещения?");
-            var time = TimeSpan.Parse(await _bot.GetCallbackQueryAsync(chatId));
-
-            // Add user to the list
-            _users.Add(new User { PeopleCount = peopleCount, VisitDate = date, VisitTime = time });
-            // Serialize to XML file
-            using (FileStream fs = new FileStream(_fileName, FileMode.Create))
+            // Send next page of the tour
+            if (_currentPage < _pages.Length)
             {
-                _serializer.Serialize(fs, _users);
+                await _bot.SendPhotoAsync(chatId, _pages[_currentPage], _captions[_currentPage]);
+                await _bot.SendMessageAsync(chatId, _texts[_currentPage]);
+                _currentPage++;
+                if (_currentPage < _pages.Length)
+                {
+                    await _bot.SendMessageAsync(chatId, "Next", replyMarkup: new InlineKeyboardMarkup(new[]
+                    {
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData("Next")
+                    }
+                }));
+                }
             }
-        }
-
-        private static async Task Feedback(long chatId)
-        {
-            // Ask for feedback
-            await _bot.SendMessageAsync(chatId, "Please leave your feedback below:");
-            var feedback = await _bot.GetCallbackQueryAsync(chatId);
-            // Save feedback to a text file
-            File.WriteAllText("feedback.txt", feedback);
-        }
-
-
-        private static async Task SignUp(long chatId)
-        {
-            // Ask for user details
-            // Add user to the list
-            // Serialize to XML file
-            var user = new User();
-            // ask user for details
-            _users.Add(user);
-            using (FileStream fs = new FileStream(_fileName, FileMode.Create))
+            else
             {
-                _serializer.Serialize(fs, _users);
+                await _bot.SendMessageAsync(chatId, "Tour has ended, thank you for visiting!");
+                _currentPage = 0;
             }
         }
     }
-}
